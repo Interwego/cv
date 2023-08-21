2899e0f7649e2c55060e0e376184c9279bccdc46
Interwego <142310001+Interwego@users.noreply.github.com>
2023-08-21 09:55:37 +0200

    Add picture, dark mode, multiple GitHub acc

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"os/exec"
+
 	"github.com/JenswBE/go-pipeline/pipeline"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	// Copy static content
+	cmd := exec.Command("cp", "-R", "static/.", "output/")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal().Err(err).Str("cmd", cmd.String()).Bytes("output", output).Msg("Failed to copy static assets")
+	}
+
 	pipeline.
 		NewHTML(nil).
 		WithOutputDir("output").
